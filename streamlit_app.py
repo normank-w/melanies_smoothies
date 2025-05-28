27798f5043350fbf5d533364948863c89bb60507
Norman Kuusik <norman.kuusik@wizon.ee>
2025-05-28 16:33:23 +0300

    C

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 helpful_links = [
@@ -21,7 +20,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -31,7 +31,7 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-if ingredients_list: 
+if ingredients_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
@@ -39,7 +39,7 @@ if ingredients_list:
 
     #st.write(ingredients_string);
     time_to_insert = st.button('Submit Order')
-    
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order +"""')"""
 
